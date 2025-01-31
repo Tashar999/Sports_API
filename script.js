e174f90d47d5bcdbe5216bb71c324073f6f1d036
Boba <98185882+BobaDude@users.noreply.github.com>
2025-01-31 13:00:16 -0500

    link in fetch function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function getData() {
-    const url = "";
+    const url = "https://api-football-v1.p.rapidapi.com/v3/teams/statistics?league=39&season=2020&team=33";
     try {
       const response = await fetch(url);
       if (!response.ok) {
