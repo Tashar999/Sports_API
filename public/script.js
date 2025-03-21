ff43867ca753df5a2291102f75f19813faa7b0af
aKungPao7urk3y <151547260+Tashar999@users.noreply.github.com>
2025-03-21 13:39:22 -0400

    carousel

--- public/script.js
+++ public/script.js
@@ -63,3 +63,51 @@ const API_URL = 'https://apiv3.apifootball.com/?action=get_standings&league_id=1
         });
       })
       .catch(error => console.error('Error fetching data:', error));
+   
+let isRunning = false;
+let slideIndex = 1;
+showSlides(slideIndex);
+      
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+      
+function showSlidesmanual(n) {
+  let i;
+  let slides = document.getElementsByClassName("Slides");
+  let dots = document.getElementsByClassName("dot");
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlidesmanual, 10000); 
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  
+      }
+
+showSlidesmanual();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("Slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 10000); 
+}
